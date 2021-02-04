4dd10bb3a9b48196f156ad1cddae331d9318a4a3
matheusF99 <mc79846@gmail.com>
2021-02-03 23:35:40 -0300

    project version 2 started

---
+++ src/pages/loginPage/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom'
+//img
+
+
+
+function Landing(){
+
+
+    return(
+
+        <div id="page-landing">
+
+            
+
+        </div>
+
+    )
+
+}
+
+export default Landing;
